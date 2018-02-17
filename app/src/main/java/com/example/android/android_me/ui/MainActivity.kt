aa6e340e246dbd035f3f5507eec1224b69b93283
jribs <josh.ribeiro3@gmail.com>
2018-02-16 21:28:17 -0500

    Add ImageClickListener for MasterListFragment

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.kt
@@ -2,15 +2,18 @@ package com.example.android.android_me.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.example.android.android_me.R
 
-/**
- * Created by E811339 on 2/15/2018.
- */
-class MainActivity: AppCompatActivity(){
+
+class MainActivity: AppCompatActivity(), MasterListFragment.OnImageClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onImageClicked(position: Int) {
+        Toast.makeText(this, "Image at position $position selected", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.kt
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.kt
@@ -1,11 +1,11 @@
 package com.example.android.android_me.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.GridView
 import com.example.android.android_me.R
 import com.example.android.android_me.data.AndroidImageAssets
 import kotlinx.android.synthetic.main.fragment_master_list.view.*
@@ -13,22 +13,31 @@ import kotlinx.android.synthetic.main.fragment_master_list.view.*
 
 class MasterListFragment: Fragment() {
 
-    lateinit var gridView: GridView
+    lateinit var imageClickListener: OnImageClickListener
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if(context is OnImageClickListener){
+            imageClickListener = context
+        } else {
+            throw RuntimeException("${context.toString()} must implement the OnImageClickListener interface")
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val layoutView = inflater?.inflate(R.layout.fragment_master_list, container, false)
         if(layoutView!=null) {
             layoutView.gridview_all_parts.adapter = MasterListAdapter(activity, AndroidImageAssets.getAll())
-            //gridView = layoutView.findViewById(R.id.gridview_all_parts)
+            layoutView.gridview_all_parts.setOnItemClickListener { adapterView, view, position, id ->
+                    imageClickListener.onImageClicked(position=position)
+            }
         }
         return layoutView
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        //If this is removed, code runs
-//        gridView.numColumns = 3
-//        gridView.adapter = MasterListAdapter(activity, AndroidImageAssets.getAll())
-
-        super.onActivityCreated(savedInstanceState)
+    interface OnImageClickListener {
+        fun onImageClicked(position: Int)
     }
+
 }
 
